dd4a353e9d4fd1c0f093bf86e8964793e1e34f19
croma <cromabar@hotmail.com>
2023-03-25 16:16:46 +0100

    Add put and delete method to animal_api.py

---
+++ routers/tests/test_animal_api.py
@@ -0,0 +1,13 @@
+# Program to test the animal API
+from fastapi.testclient import TestClient
+from db.models.animal import Animal
+from gestorAnimales.main import app
+
+client = TestClient(app)
+
+# Test the get_animals function
+def test_get_animals():
+    response = client.get("/animals/")
+    assert response.status_code == 200
+    assert response.json() == list(Animal)
+    
